0f71d4be2513f3b5b2fc5a6ed48550fff2e74d23
Mike Burr <mb@unintuitive.org>
2022-04-24 09:20:28 -0600

    .

--- ports/esp32/boards/ESP32_S3_EYE/_deploy.sh
+++ ports/esp32/boards/ESP32_S3_EYE/_deploy.sh
@@ -11,4 +11,17 @@ $make submodules
 $make
 
 # maybe...
-## /Users/mburr/.espressif/python_env/idf4.4_py3.9_env/bin/python ../../../esp-idf/components/esptool_py/esptool/esptool.py -p (PORT) -b 460800 --before default_reset --after no_reset --chip esp32s3  write_flash --flash_mode dio --flash_size detect --flash_freq 80m 0x0 build-ESP32_S3_EYE/bootloader/bootloader.bin 0x8000 build-ESP32_S3_EYE/partition_table/partition-table.bin 0x10000 build-ESP32_S3_EYE/micropython.bin
+echo /Users/mburr/.espressif/python_env/idf4.4_py3.9_env/bin/python \
+     ../../../esp-idf/components/esptool_py/esptool/esptool.py \
+     -p $TTY \
+     -b 460800 \
+     --before default_reset \
+     --after no_reset \
+     --chip esp32s3 \
+     write_flash \
+     --flash_mode dio \
+     --flash_size detect \
+     --flash_freq 80m \
+     0x0 build-ESP32_S3_EYE/bootloader/bootloader.bin \
+     0x8000 build-ESP32_S3_EYE/partition_table/partition-table.bin \
+     0x10000 build-ESP32_S3_EYE/micropython.bin
