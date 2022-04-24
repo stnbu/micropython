8bd63f74efcdbec70c4456ade87927d840b6304f
Mike Burr <mb@unintuitive.org>
2022-04-24 09:35:26 -0600

    works

--- ports/esp32/boards/ESP32_S3_EYE/_deploy.sh
+++ ports/esp32/boards/ESP32_S3_EYE/_deploy.sh
@@ -1,17 +1,18 @@
-#!/bin/sh -xue
+#!/bin/sh -ue
 
 PORTS_DIR=$(dirname $0)/../..
 cd $PORTS_DIR
 
-TTY=$(ls /tmp/tty.usbmodem*)
+TTY=$(ls /dev/tty.usbmodem*)
 
 make="make BOARD=ESP32_S3_EYE PORT=$TTY"
 
 $make submodules
 $make
+$make deploy
 
 # maybe...
-echo /Users/mburr/.espressif/python_env/idf4.4_py3.9_env/bin/python \
+true ~/.espressif/python_env/idf4.4_py3.9_env/bin/python \
      ../../../esp-idf/components/esptool_py/esptool/esptool.py \
      -p $TTY \
      -b 460800 \
@@ -25,3 +26,6 @@ echo /Users/mburr/.espressif/python_env/idf4.4_py3.9_env/bin/python \
      0x0 build-ESP32_S3_EYE/bootloader/bootloader.bin \
      0x8000 build-ESP32_S3_EYE/partition_table/partition-table.bin \
      0x10000 build-ESP32_S3_EYE/micropython.bin
+
+# transfer files?
+# flash "over wifi"?
